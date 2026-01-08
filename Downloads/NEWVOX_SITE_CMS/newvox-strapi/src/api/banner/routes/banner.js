c39717585640bcdf1d8d2474d5ea9560d905fc59
tulioalvex <tulioalvex@gmail.com>
2026-01-08 08:14:36 -0300

    fix: update banner routes handler

--- Downloads/NEWVOX_SITE_CMS/newvox-strapi/src/api/banner/routes/banner.js
+++ Downloads/NEWVOX_SITE_CMS/newvox-strapi/src/api/banner/routes/banner.js
@@ -1,10 +1,30 @@
 'use strict';
 module.exports = {
   routes: [
-    { method: 'GET', path: '/banners', handler: 'banner.find' },
-    { method: 'GET', path: '/banners/:id', handler: 'banner.findOne' },
-    { method: 'POST', path: '/banners', handler: 'banner.create' },
-    { method: 'PUT', path: '/banners/:id', handler: 'banner.update' },
-    { method: 'DELETE', path: '/banners/:id', handler: 'banner.delete' },
+    {
+      method: 'GET',
+      path: '/banners',
+      handler: 'api::banner.banner.find',
+    },
+    {
+      method: 'GET',
+      path: '/banners/:id',
+      handler: 'api::banner.banner.findOne',
+    },
+    {
+      method: 'POST',
+      path: '/banners',
+      handler: 'api::banner.banner.create',
+    },
+    {
+      method: 'PUT',
+      path: '/banners/:id',
+      handler: 'api::banner.banner.update',
+    },
+    {
+      method: 'DELETE',
+      path: '/banners/:id',
+      handler: 'api::banner.banner.delete',
+    },
   ],
 };
