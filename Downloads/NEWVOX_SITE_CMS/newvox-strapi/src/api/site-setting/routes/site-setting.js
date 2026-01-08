0fe49b3d8d50e720e831a1f4444c7eb09a7a2ec8
tulioalvex <tulioalvex@gmail.com>
2026-01-08 10:01:53 -0300

    Fix site-setting routes for single type (remove :id)

--- Downloads/NEWVOX_SITE_CMS/newvox-strapi/src/api/site-setting/routes/site-setting.js
+++ Downloads/NEWVOX_SITE_CMS/newvox-strapi/src/api/site-setting/routes/site-setting.js
@@ -1,10 +1,9 @@
 'use strict';
+
 module.exports = {
   routes: [
+    // Single Type: não usa :id
     { method: 'GET', path: '/site-settings', handler: 'api::site-setting.site-setting.find' },
-    { method: 'GET', path: '/site-settings/:id', handler: 'api::site-setting.site-setting.findOne' },
-    { method: 'POST', path: '/site-settings', handler: 'api::site-setting.site-setting.create' },
-    { method: 'PUT', path: '/site-settings/:id', handler: 'api::site-setting.site-setting.update' },
-    { method: 'DELETE', path: '/site-settings/:id', handler: 'api::site-setting.site-setting.delete' },
+    { method: 'PUT', path: '/site-settings', handler: 'api::site-setting.site-setting.update' },
   ],
 };
