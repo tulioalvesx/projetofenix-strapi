b85ec34b7e20e5168dd90ca1d8944f43102c8a2d
tulioalvex <tulioalvex@gmail.com>
2026-01-08 07:58:08 -0300

    fix: postgres ssl rejectUnauthorized false

--- Downloads/NEWVOX_SITE_CMS/newvox-strapi/config/database.js
+++ Downloads/NEWVOX_SITE_CMS/newvox-strapi/config/database.js
@@ -1,14 +1,15 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: env('DATABASE_CLIENT', 'postgres'),
+    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'newvox'),
-      user: env('DATABASE_USERNAME', 'newvox'),
-      password: env('DATABASE_PASSWORD', 'newvox'),
-      ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
+      connectionString: env('DATABASE_URL'),
+      ssl: env.bool('DATABASE_SSL', true) && {
+        rejectUnauthorized: false,
+      },
     },
-    pool: { min: 0, max: 10 },
+    pool: {
+      min: 0,
+      max: 10,
+    },
   },
 });
